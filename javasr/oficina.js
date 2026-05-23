29ec19d252d52bc605206b062aafe0f1e86ef346
Esther Abigail Coronado Ckacka <coronadoabigail777@gmail.com>
2026-05-23 03:33:38 -0400

    subiendo unidad administrativa

---
+++ javasr/oficina.js
@@ -0,0 +1,70 @@
+// oficina.js
+const url = "http://127.0.0.1:3000/oficina";
+function nuevo(){
+    document.getElementById("txtOficina").value = "";
+    document.getElementById("txtObservacion").value = "";
+    alert("nuevo registro");
+}
+function guardar(){
+    let oficina = document.getElementById("txtOficina").value;
+    let observacion = document.getElementById("txtObservacion").value;
+    if(oficina == ""){
+        alert("ingrese oficina");
+        return;
+    }
+    fetch(url,{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            oficina:oficina,
+            observacion:observacion
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        alert("guardado");
+    });
+}
+function modificar(){
+
+    let oficina = document.getElementById("txtOficina").value;
+
+    fetch(url+"/1",{
+        method:"PUT",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            oficina:oficina
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        alert("modificado");
+    });
+}
+function activar(){
+    alert("activado");
+}
+function inactivar(){
+
+    fetch(url+"/1",{
+        method:"DELETE"
+    })
+    .then(()=>{
+        alert("inactivado");
+    });
+
+}
+function deshacer(){
+    document.getElementById("txtOficina").value = "";
+    document.getElementById("txtObservacion").value = "";
+    alert("deshecho");
+}
+function salir(){
+    alert("saliendo");
+}
